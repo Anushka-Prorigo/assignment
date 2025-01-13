b31775d13ca06a75aa94ce62664ffcbb34d8d2a6
Anushka Patil <anushka.patil@prorigosoftware.com>
2025-01-13 10:23:59 +0530

    changes in application 2

--- Application2/app/src/main/java/com/example/application2/WebViewActivity.java
+++ Application2/app/src/main/java/com/example/application2/WebViewActivity.java
@@ -2,55 +2,72 @@ package com.example.application2;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Handler;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WebViewActivity extends AppCompatActivity {
 
     private WebView webview;
-    private boolean isCredentialsInjected = false;
+    private boolean isSignInPageLoaded = false;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
         webview = findViewById(R.id.webview);
-
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
         webview.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageFinished(WebView view, String url)
-            {
+            public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                inputCredentials();
+                Log.e("TAG", "after gmail display");
 
+                if(isSignInPageLoaded==false)
+                {
+                    isSignInPageLoaded=true;
+                    Log.e("TAG", "Loading sign-in page");
+                    loadSignInPage();
+                }
+
+                if (isSignInPageLoaded==true)
+                {
+                    Log.e("TAG", "Autofilling credentials");
+                    isSignInPageLoaded = true;
+                    autofillCredentials();
+                }
             }
         });
 
-        webview.loadUrl("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ddm=1&flowEntry=ServiceLogin&flowName=GlifWebSignIn&ifkv=AeZLP98eKWHhnox5mQNsMytHCHTHOLfs71EI3g1INvL-XR0b4KhtNio16MHgX_m-z8UHpL0ZBLKZKA&rip=1&service=mail&dsh=S1058792472%3A1736512733583558");
+        Log.e("TAG", "gmail display");
+        webview.loadUrl("https://mail.google.com");
     }
-    private void inputCredentials()
+
+    private void loadSignInPage()
     {
-        String username = getIntent().getStringExtra("username");
+        Log.e("TAG", "Navigating to sign-in page");
+        webview.loadUrl("https://accounts.google.com/ServiceLogin?service=mail");
+    }
+
+    private void autofillCredentials()
+    {
+        String email = getIntent().getStringExtra("username");
         String password = getIntent().getStringExtra("password");
 
-        String jsCode = "javascript:(function() {" +
-                "document.querySelector('input[type=email]').value = '" + username + "';" +
-                "document.querySelector('input[type=password]').value = '" + password + "';" +
-
-                "})()";
-
-        webview.evaluateJavascript(jsCode, null);
+        webview.evaluateJavascript("javascript:(function() {" +
+                "document.querySelector('input[type=\"email\"]').value = '" + email + "';" +
+                "document.querySelector('input[type=\"email\"]').dispatchEvent(new Event('input'));" +
+                "document.querySelector('input[type=\"password\"]').value = '" + password + "';" +
+                "document.querySelector('input[type=\"password\"]').dispatchEvent(new Event('input'));" +
+                "document.querySelector('button[jsname=\"LgbsSe\"]').click();" +
+                "})()", null);
     }
 }
